21191df63b25e50ee6c84386ca7806c9724ab07f
Macbook <macbook@mac-cua-mvi.local>
2024-09-11 10:54:28 +0700

    cap nhat

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const multer  = require('multer')
-const router = express.Router()
-const storageMulter = require("../../helper/storageMulter")
 
-const upload = multer({ storage: storageMulter() })
+const router = express.Router()
+
+const upload = multer()
 
 const controller = require('../../controllers/admin/product.controller')
 const validate = require("../../validates/admin/product.validate")
 
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
+ 
 router.get('/', controller.index)
 
 router.patch('/change-status/:status/:id', controller.changeStatus)
@@ -18,7 +20,11 @@ router.delete('/delete/:id', controller.deleteItem)
 
 router.get('/create', controller.create)
 
-router.post('/create', upload.single('thumbnail'), validate.createPost, controller.createPost)
+router.post('/create', 
+    upload.single('thumbnail'),
+    uploadCloud.upload,
+    validate.createPost, 
+    controller.createPost)
 
 router.get('/edit/:id', controller.edit)
 
